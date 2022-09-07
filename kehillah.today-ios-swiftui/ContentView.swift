5a1e196356b5c7d3bbc6e6661a98c8ac08514af3
Ovlic <74332950+Ovlic@users.noreply.github.com>
2022-09-07 09:50:07 -0700

    Initial Commit

---
+++ kehillah.today-ios-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kehillah.today-ios-swiftui
+//
+//  Created by Ovlic B on 9/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kehillah.today-ios-swiftui/kehillah_today_ios_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kehillah_today_ios_swiftuiApp.swift
+//  kehillah.today-ios-swiftui
+//
+//  Created by Ovlic B on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct kehillah_today_ios_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
