c92e1532e85fa415905032112edd826675ba7ce6
Ovlic <74332950+Ovlic@users.noreply.github.com>
2022-11-18 09:26:26 -0800

    Add workflows

---
+++ .github/workflows/app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eo pipefail
+
+xcodebuild -workspace Calculator.xcworkspace \
+            -scheme Calculator\ iOS \
+            -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
+            clean test | xcpretty
+view raw
