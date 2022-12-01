864bbaba6dd2c29bad84f421f11295f5bb32ed4f
Ovlic <74332950+Ovlic@users.noreply.github.com>
2022-12-01 12:47:12 -0800

    Delete app.sh

--- .github/workflows/app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -workspace Calculator.xcworkspace \
-            -scheme Calculator\ iOS \
-            -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
-            clean test | xcpretty
-view raw
